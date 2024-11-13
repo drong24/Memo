cd2e809c0e8747e4e3dc9a09f2c9c3af3c5f4511
drong24 <daisyrong24@gmail.com>
2024-11-13 15:33:31 -0800

    added xcode project to repository

---
+++ Memo/Memo/MemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoApp.swift
+//  Memo
+//
+//  Created by Daisy Rong on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
